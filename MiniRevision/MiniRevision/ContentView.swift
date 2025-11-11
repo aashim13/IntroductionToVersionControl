46b5551b832a94141ddf0861c379a95b73548f90
aashim13 <aa.shim130@gmail.com>
2025-11-11 09:51:16 +0100

    First Commit

---
+++ MiniRevision/MiniRevision/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniRevision
+//
+//  Created by Aashi Mehrotra on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniRevision/MiniRevision/MiniRevisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniRevisionApp.swift
+//  MiniRevision
+//
+//  Created by Aashi Mehrotra on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniRevisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
